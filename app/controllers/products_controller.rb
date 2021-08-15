f5dc2ab4f2e987b3d95a5441a396ca557d8095ae
Adejare Toluwalope <standardblaise@gmail.com>
2021-08-16 00:17:17 +0100

    Adding the  product page..

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
-class ProductController < ApplicationController
+class ProductsController < ApplicationController
 
     def index
-        @category = category.find(params[category_id])
+        @category = Category.find(params[:category_id])
         @products = @category.products.includes(:variants).order(:title)
     end
 
