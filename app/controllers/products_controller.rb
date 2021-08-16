16bf58d56a1b832c30cf415efc3465bdea2c4243
Adejare Toluwalope <standardblaise@gmail.com>
2021-08-17 00:26:08 +0100

    Fixing the product display error.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
     def index
         @category = Category.find(params[:category_id])
-        @products = @category.products.includes(:variants).order(:title)
+        @products = @category.products.includes(:product_variants).order(:title)
     end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
 
     has_many :product_categories
     has_many :categories, through: :product_categories
+    has_many :product_variants
 end
