17157754c1a2727f2d94b8f0a13ed4864b7940a0
Adejare Toluwalope <standardblaise@gmail.com>
2021-10-11 01:21:07 +0100

    Using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    Product.create!([{
+        title: Faker::Commerce.product_name,
+        categories: Faker::Commerce.department(max:, 5)
+        variant: Faker::Commerce.material,
+        price: Faker::Number.between(from: 50, to: 2000)
+    }])
+end
